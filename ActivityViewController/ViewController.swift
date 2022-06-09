ac5aab2b2ce3244fd5fa8a2d603e84f61be0b261
Мигдисов Всеволод <vmigdisov@it-one.ru>
2022-06-09 21:19:03 +0300

    Initial commit

--- ActivityViewController/ViewController.swift
+++ ActivityViewController/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private lazy var button: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .lightGray
+        button.setTitle("UIActivityViewController", for: .normal)
+        button.sizeToFit()
+        button.center = view.center
+        button.addTarget(self, action: #selector(buttonTap), for: .touchUpInside)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        view.addSubview(button)
     }
 
-
+    @objc func buttonTap() {
+        guard let url = URL(string: "https://www.apple.com") else { return }
+        let ac = UIActivityViewController(activityItems: [url], applicationActivities: nil)
+        present(ac, animated: true)
+    }
 }
 
