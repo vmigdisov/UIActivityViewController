c785a1abcb704b7dd10ebaef60aa16be1333d522
Мигдисов Всеволод <vmigdisov@it-one.ru>
2022-06-09 16:46:24 +0300

    Initial Commit

---
+++ ActivityViewController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ActivityViewController
+//
+//  Created by Vsevolod Migdisov on 09.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
